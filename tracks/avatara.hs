91cff58f97a1750f9bc72328a61edfadcd808be1
Ebmtranceboy <ebmtranceboy@gmail.com>
2017-03-13 16:38:06 +0100

    +avataria+zenrush

---
+++ tracks/avatara.hs
@@ -0,0 +1,78 @@
+{- AVATARA aka loop-102-}
+import Csound.Patch
+import Csound.Base
+
+pace = 40
+iter = 10
+notes pchs = map (\pch -> (0.5, cpspch pch)) pchs
+sequencer instr tempo xs  = mix $ str tempo $ loopBy iter $ atSco instr $ mel 
+           $ fmap temp $ notes $ concat xs
+
+avatar = 
+   let oct = 8
+       ins pch = temp (0.1, cpspch $ oct + pch)
+       tick n = 60 * n / pace
+       in mix $ loopBy iter $ atSco (avatara 10)
+                           $ har [ mel $ zipWith str (map tick [5,1,7,1,2]) 
+                                            $ map ins [0.05,0.04,0.05,0.04,0.05]
+                                 , mel $ zipWith str (map tick [2,2,2,2,6,2]) 
+                                            $ map ins [0.10,0.08,0.07,0.08,0.10,0.08]
+                                 , mel $ zipWith str (map tick [1,1,7,1,2,4]) 
+                                            $ map ins [1.00,1.01,1.00,1.03,1.01,1.00]
+                                 ] 
+     
+synth = 
+   let oct = 8
+       ins pch = temp (0.5, cpspch $ oct + pch)
+       tick n = 60 * 16 / 96 * n / pace
+       in mix $ loopBy iter $ atSco razorLeadFast
+                           $ mel $ (++ [rest $ tick 4]) $ zipWith str (map tick 
+                                                         [6,2,2,2,11
+                                                         ,1,5,1,5,1,8
+                                                         ,2,2,7,2,2,2,11
+                                                         ,1,4,1,5,1,8]) 
+                                         $ map ins [0.10,1.00,1.01,1.03,1.05
+                                                   ,1.05,1.04,1.05,1.00,0.10,0.08
+                                                   ,0.00,0.01,0.10,1.00,1.01,1.03,1.01
+                                                   ,1.00,0.07,0.05,0.08,0.07,0.05]
+                                 
+canon = 
+   let oct = 8
+       ins pch = temp (0.4, cpspch $ oct + pch)
+       tick n = 60 * 16 / 96 * n / pace
+       in mix $ loopBy iter $ atSco razorLead
+                           $ mel $ zipWith str (map tick 
+                                              [ 5,1,5,1,5,1,5,1,5,1,5,1,12
+                                              , 5,1,5,1,5,1,5,1,5,1,5,1,12
+                                              ]) 
+                                   $ map ins [0.05,1.00,0.10,0.08,0.07
+                                             ,0.05,0.07,0.08,0.07,0.05,0.04
+                                             ,0.05,1.00
+                                             ,0.05,0.08,0.07,0.05,1.01
+                                             ,0.10,0.08,0.07,0.05,0.07,0.08,0.10,1.00]
+
+pad =                                                    
+   let oct = 7
+       ins pch = temp (0.08, cpspch $ oct + pch)
+       tick n = 60 * 16 / 16 * n / pace
+       in mix $ loopBy iter $ atSco polySynth
+                   $ har [ mel $ zipWith str (map tick [1,1,2,1,1,2,1,1,2,1,1,2 ]) 
+                                       $ map ins 
+                              [ 0.10,1.03,1.05
+                              , 1.04,1.00,1.05
+                              , 0.10,1.01,1.05
+                              , 1.04,1.00,1.05
+                              ]
+                                 , mel $ zipWith str (map tick [2,2,2,2,4,2,2])
+                                  $ map ins 
+                                 [1.10,1.08,1.07,1.08,1.10,1.07,1.08]]
+
+main = dac $ {- at (dumpWav "avatara.wav") $ -} sum $ [ atMidi polySynth
+                   ,return avatar
+                   ,return synth
+                   ,return canon
+                   ,return pad
+                   ]
+
+
+                  
---
+++ tracks/zenrush.hs
@@ -0,0 +1,91 @@
+
+{- ZENRUSH aka loop-108 -}
+import Csound.Patch
+import Csound.Base
+
+iter = 13
+pace = 3.7
+notes pchs = map (\pch -> (0.5, cpspch pch)) pchs
+sequencer instr tempo xs  = 
+       mix $ str (1/tempo) $ loopBy iter $ atSco instr $ mel 
+           $ fmap temp $ notes $ concat xs
+           
+bass oct = let doct = 1 + oct in sequencer razorLead pace [
+             arp1 [oct+0.08] (doct+0.05,doct+0.08,doct+1.00)
+           , arp2 [oct+1.02] (doct+0.05,doct+0.08,doct+1.00)
+           , arp1 [oct+1.00] (doct+0.04,doct+0.07,doct+1.00) 
+           , arp2 [oct+0.07] (doct+0.04,doct+0.07,doct+0.10)
+           , arp1 [oct+0.08] (doct+0.05,doct+0.08,doct+1.00) 
+           , arp2 [oct+0.05] (doct+0.05,doct+0.08,doct+1.00)
+           , arp1 [oct+0.00] (doct+0.04,doct+0.07,doct+1.00) 
+           , arp2 [oct+0.02] (doct+0.02,doct+0.05,doct+0.10)
+           ]
+
+rush = let oct = 9 
+        in sequencer ({-FxChain [FxSpec 0.75 (return . smallHall2)]-} polySynth) (2*pace) [
+              arp3 (oct+0.05,oct+0.08,oct+1.00)
+            , arp3 (oct+0.05,oct+0.08,oct+1.02)
+            , arp3 (oct+0.04,oct+0.07,oct+1.00)
+            , arp3 (oct+0.04,oct+0.07,oct+1.02)
+            
+            , arp3 (oct+0.05,oct+0.08,oct+1.00)
+            , arp3 (oct+0.05,oct+0.08,oct+1.02)
+            , arp3 (oct+0.04,oct+0.07,oct+1.00)
+            , arp3 (oct+0.02,oct+0.10,oct+1.05)
+            ]
+
+satellite = let oct = 10 
+            in sequencer banyan (pace/4) [[oct+0.08,oct+1.08,oct+1.03,oct+1.05,
+                                      oct+0.07,oct+1.07,oct+1.02,oct+1.04,
+                                      oct+0.08,oct+1.08,oct+1.03,oct+1.05,
+                                      oct+0.07,oct+1.07,oct+1.04,oct+1.05
+                                      ]]
+arp1 [f] (a,b,c) = [f,a,b,c,f,b,a,c]
+arp2 [f] (a,b,c) = [f,b,a,b,f,a,c,b]
+arp3 (a,b,c) = concat $ replicate 4 [b,a,c,a]
+
+melody = 
+   let oct = 7
+       ins pch = (0.7, cpspch $ oct + pch)
+       ticks n = n / pace /2
+       in mix $ loopBy iter $ atSco polySynth 
+                           $ mel $ ( ++ [rest $ ticks 8]) 
+                           $ zipWith str (map ticks 
+                            [1,1,2,8
+                            ,2,1,1,8
+                            ,1,1,2,2,4
+                            ,1,1,2,2,2,12
+                            ,2,2,8]) 
+                                 $ zipWith del [16, 0,0,0
+                                               ,8, 0,0,0
+                                               ,20, 0,0,0,0
+                                               ,2, 0,0,0,0,0
+                                               ,0,0,0
+                                               ] 
+                                 $ map (temp . ins) [0.05,0.07,0.08,1.00
+                                                    ,0.04,0.05,0.07,1.00
+                                                    ,0.08,0.10,1.00,1.05,1.00
+                                                    ,1.00,1.05,1.08,1.10,1.08,1.04
+                                                    ,1.02,1.00,1.07
+                                                    ]
+
+aout = bass 6 + satellite
+main = dac $ sum [melody
+                 ,rush
+                 ,aout
+                 ]
+                                                     
+{-
+
+main = dac $ sum [return melody
+                 --,return $ bass 6
+                , return rush
+                 --, return satellite
+                  ,atMidi polySynth
+                 ]
+
+main = dac $ sum $ [mul 0.5 $ loopWav 1 "../../../Desktop/perso/loops/loop-108-rush.wav"
+                   , loopWav (1) "../../../Desktop/perso/loops/loop-108.wav"
+                   , mul 1.5 $ loopWav (1) "../../../Desktop/perso/loops/loop-108-piano.wav"
+                   ]
+-}
